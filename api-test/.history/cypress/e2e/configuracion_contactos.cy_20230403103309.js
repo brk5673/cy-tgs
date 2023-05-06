b951da3c27a729f92e66e74e20f48f2496126ada
brk5673 <97960640+brk5673@users.noreply.github.com>
2023-05-06 19:17:48 -0300

    contactsConfig us1120

--- api-test/.history/cypress/e2e/configuracion_contactos.cy_20230403103309.js
+++ api-test/.history/cypress/e2e/configuracion_contactos.cy_20230403103309.js
@@ -20,17 +20,31 @@ describe('Test cases de modulo Estado de Sistema', () => { // corregir describe
     })
 
     it('us1120 - Validacion del menu, pantalla y carga inicial', () => {
+      //dirigirse al punto configContactos por UI
         cy.visit('http://10.1.11.237:8080/etgs/')
         cy.contains('span.MuiListItemText-primary', 'SPAC').click()
         cy.contains('span.MuiListItemText-primary', 'Mantenimiento').click()
         cy.contains('span.MuiListItemText-primary', 'Desvío de Inyección').click()
         cy.contains('span.MuiListItemText-primary', 'Configuración de contactos').click()
-
+      
+      // dirigirse a la pagina y validar el titulo en el headers
       //  cy.url().should('eq', 'http://10.1.11.237:8080/etgs/spac/mantenimiento/desviodeinyeccion/configuraciondecontactos')
       //  cy.get('#navPath').contains('Configuración de contactos', {matchCase: false}).should('exist') // validar nombre el headers
+        
+      // valiar columnas de tabla
         cy.contains('Gasoducto').should('exist')
         cy.contains('Entidad Legal').should('exist')
         cy.contains('Contacto').should('exist')
+
+      
     })
+
+    it('us1120 - Validacion del menu, pantalla y carga inicial', () => {
+    //dirigirse al punto configContactos
+
+
+      
+    })
+
 
 })  
