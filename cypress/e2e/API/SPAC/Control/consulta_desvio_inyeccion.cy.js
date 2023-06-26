9e7dee4adbf5c1a6194164f2ce868d29a3c084bd
brk5673 <97960640+brk5673@users.noreply.github.com>
2023-06-26 16:30:30 -0300

    note cdi

---
+++ cypress/e2e/API/SPAC/Control/consulta_desvio_inyeccion.cy.js
@@ -0,0 +1,124 @@
+/// <reference types="cypress"/>
+import { PASS1, USER1 } from "../../../../fixtures/credentials"
+
+describe('API tests <Consulta de Desvio de Inyecciones> module', () => {
+    beforeEach(() => {
+        cy.loginAPI(USER1, PASS1)
+    })
+
+    it('[init] status 200, response properties', () => {
+        cy.request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/init'
+        })
+        .then((response) => {
+            // assertion status200 api response
+            expect(response.status).to.eq(200)
+            // response to have property 'fechaminima' & YYYY-MM-DD format
+            expect(response.body).to.have.property('fechaMinima')
+            expect(response.body.fechaMinima).to.match(/\d{4}-\d{2}-\d{2}/)
+            // response to have property 'fechamaxima' & YYYY-MM-DD format
+            expect(response.body).to.have.property('fechaMaxima')
+            expect(response.body.fechaMaxima).to.match(/\d{4}-\d{2}-\d{2}/)
+            // response to have property 'fechaDefaultSelected'
+            expect(response.body).to.have.property('fechaFinalDefaultSelected')
+            expect(response.body).to.have.property('fechaInicialDefaultSelected')
+            
+        })
+    })
+
+    it('[listar] status 200, properties', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/?fechaDesde=2021-06-21T06:00:00&fechaHasta=2021-06-21T06:00:00'
+        })
+        .then((response) => {
+            // assertion with api response
+            expect(response.status).to.eq(200)
+            // expect 'body.errores' contain string not equal to zero
+            expect(response.body['errores']).not.to.equal('0')
+        })
+    })
+
+    it('[listar] status code 400, ', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/?fechaDesde=2021-06-21T06:00:00&fechaHasta=2021-06-203T05:00:00',
+            failOnStatusCode: false
+        })
+        .then((response) => {
+            // assertion with api response
+            expect(response.status).to.eq(400)
+        })
+    })
+
+    it('[get nota <NOTA 1 CON SUPERACION>] status200', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/nota-desvio?gasoducto=310&operador=111&tipoNota=NOTA_1_CON_SUPERACION&fechaDesde=2020-06-21T06:00:00&fechaHasta=2020-06-21T09:00:00'
+        }).then((response) => {
+            // assertion on api response
+            expect(response.status).to.eq(200)
+            // expect 'body.errores' contain string not equal to zero
+            expect(response.body['errores']).not.to.equal('0')
+        })
+    })
+
+    it('[get nota <NOTA 2 CON SUPERACION>] status200', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/nota-desvio?gasoducto=310&operador=111&tipoNota=NOTA_2_CON_SUPERACION&fechaDesde=2020-06-21T06:00:00&fechaHasta=2020-06-21T09:00:00'
+        }).then((response) => {
+            // assertion on api response
+            expect(response.status).to.eq(200)
+            // expect 'body.errores' contain string not equal to zero
+            expect(response.body['errores']).not.to.equal('0')
+        })
+    })
+
+    it('[get nota <NOTA 1 SIN SUPERACION>] status200', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/nota-desvio?gasoducto=310&operador=111&tipoNota=NOTA_1_SIN_SUPERACION&fechaDesde=2020-06-21T06:00:00&fechaHasta=2020-06-21T09:00:00'
+        }).then((response) => {
+            // assertion on api response
+            expect(response.status).to.eq(200)
+            // expect 'body.errores' contain string not equal to zero
+            expect(response.body['errores']).not.to.equal('0')
+        })
+    })
+
+    it('[get nota <NOTA 2 SIN SUPERACION>] status200', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/nota-desvio?gasoducto=310&operador=111&tipoNota=NOTA_2_SIN_SUPERACION&fechaDesde=2020-06-21T06:00:00&fechaHasta=2020-06-21T09:00:00'
+        }).then((response) => {
+            // assertion on api response
+            expect(response.status).to.eq(200)
+            // expect 'body.errores' contain string not equal to zero
+            expect(response.body['errores']).not.to.equal('0')
+        })
+    })
+
+    it('[get nota <NOTA DEFICIT DE INYECCION>] status200', () => {
+        cy.get('@jsession').request({
+            method: 'GET',
+            url: '/api/spac/control/desvio-inyeccion/nota-desvio?gasoducto=310&operador=111&tipoNota=NOTA_DEFICIT_DE_INY&fechaDesde=2020-06-21T06:00:00&fechaHasta=2020-06-21T09:00:00'
+        }).then((response) => {
+            // assertion on api response
+            expect(response.status).to.eq(200)
+            // expect 'body.errores' contain string not equal to zero
+            expect(response.body['errores']).not.to.equal('0')
+        })
+    })
+
+
+    
+
+
+
+
+        
+
+
+})
