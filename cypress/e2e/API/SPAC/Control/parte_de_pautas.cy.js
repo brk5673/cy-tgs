3baa1c650ed6000638b88d8627d0ce6d8941ee97
brk5673 <97960640+brk5673@users.noreply.github.com>
2023-06-26 11:55:40 -0300

    pdp enviar

--- cypress/e2e/API/SPAC/Control/parte_de_pautas.cy.js
+++ cypress/e2e/API/SPAC/Control/parte_de_pautas.cy.js
@@ -57,6 +57,55 @@ describe('API tests <Parte de Pautas> module', () => {
         })
     })
 
+    it.only('[enviar] status 200 + response properties', () => {
+        cy.get('@jsession').request({
+            method: 'POST',
+            url: '/api/spac/control/partedepautas/enviar?fecha=2021-06-21',
+            body: {
+                "fechaGeneracion": "2020-05-14T14:30:00",
+                "fechaOperativo": "2020-05-14",
+                "recepGlobales": {
+                  "cuencaSurProgramada": 1234,
+                  "cuencaSurRealizada": 1234,
+                  "cuencaNeuquinaProgramada": 1234,
+                  "cuencaNeuquinaRealizada": 1234
+                },  
+                "estadoSistemaOperativo": {
+                  "estado": "String",
+                  "motivo": "String"
+                },
+                "linePack": {
+                  "diaAnterior": 123456.123,
+                  "diaActual": 123456.123,
+                  "variacion": 123456.123
+                },
+                "poderCalorificoTramoGasoducto": {
+                  "sanMartinLlegada": 1234,
+                  "neubaILlegada": 1234,
+                  "neubaIILlegada": 1234,
+                  "gutierrezLlegada": 1234,
+                  "buchananILlegada": 1234,
+                  "buchananIILlegada": 1234,
+                  "troncalSalida": 1234,
+                  "paraleloSalida": 1234,
+                  "neubaIISalida": 1234,
+                  "rodriguezSalida": 1234,
+                  "pachecoSalida": 1234,
+                  "ezeizaSalida": 1234
+                },  
+                "observaciones": "String"
+              }
+        })
+        .then((response) => {
+            // assertion with api response
+            expect(response.status).to.eq(200)
+            //expect response headers contain value file.pdf
+            expect(response.headers['content-type']).to.contain('pdf')
+            // expect 'content length' contain string not equal to zero
+            expect(response.headers['content-length']).not.to.equal('0')
+        })
+    })
+
 
 
 
