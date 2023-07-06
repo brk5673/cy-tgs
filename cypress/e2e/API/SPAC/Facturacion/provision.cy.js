905f549298c71be034ab8f956153be342c7df48f
brk5673 <97960640+brk5673@users.noreply.github.com>
2023-07-06 17:46:51 -0300

    fact

--- cypress/e2e/API/SPAC/Facturacion/provision.cy.js
+++ cypress/e2e/API/SPAC/Facturacion/provision.cy.js
@@ -21,56 +21,81 @@ describe('API tests <Provision> module', () => {
         })
     })
 
-/*     it('[generar provision] status 200', () => {
+
+   /*  it('[reporte <pdf>] status 200', () => {
         cy.get('@jsession').request({
             method: 'POST',
-            url: '/api/spac/facturacion/provision/generarprovision?periodo=2023%2F02',
+            url: '/api/spac/facturacion/provision/generarReporte',
+            body: {
+                "errores": {
+                  "lista": [
+                    {
+                      "atributosExtra": {
+                        "additionalProp1": "string",
+                        "additionalProp2": "string",
+                        "additionalProp3": "string"
+                      },
+                      "descripcion": "string",
+                      "id": 0,
+                      "tipoError": "ERROR"
+                    }
+                  ]
+                },
+                "itemsAperturaImpositiva": [
+                  {
+                    "camino": "string",
+                    "caminoCorto": "string",
+                    "catIva": "string",
+                    "codigoSociedad": "string",
+                    "comprobante": "string",
+                    "contrato": "string",
+                    "entidadLegal": "string",
+                    "fecha": "string",
+                    "oferta": "string",
+                    "provincia": "string",
+                    "provinciaIngresosBrutos": "string",
+                    "provinciaIva": "string",
+                    "provinciaPercepcionIngresosBrutos": "string",
+                    "ruta": "string",
+                    "signo": "string",
+                    "tipoMercado": "string",
+                    "volumen": 0,
+                    "zonaEntrega": "string",
+                    "zonaOrigen": "string",
+                    "zonaRecepcion": "string"
+                  }
+                ],
+                "itemsOriginal": [
+                  {
+                    "camino": "string",
+                    "caminoCorto": "string",
+                    "codigoContrato": "string",
+                    "codigoRuta": "string",
+                    "codigoSigno": "string",
+                    "codigoSociedad": "string",
+                    "codigoZonaEntrega": "string",
+                    "codigoZonaOrigen": "string",
+                    "codigoZonaRecepcion": "string",
+                    "comprobante": "string",
+                    "fecha": "string",
+                    "numeroEntidadLegal": "string",
+                    "oferta": "string",
+                    "tipoMercado": "string",
+                    "volumen": 0
+                  }
+                ],
+                "periodo": "2022/09"
+              }
         })
         .then((response) => {
             expect(response.status).to.eq(200)
+            //expect response headers contain value file.pdf
+            expect(response.headers['content-disposition']).to.contain('.pdf')
         })
     
-    
-    
+
     })
  */
-
-    it('[disponibles] status 200', () => {
-        cy.get('@jsession').request({
-            method: 'GET',
-            url: '/api/spac/facturacion/facturacion/disponibles',
-        })
-        .then((response) => {
-            expect(response.status).to.eq(200)
-            console.log(response.body)
-        })
-    })
-
-    it('[en revision] status 200', () => {
-        cy.get('@jsession').request({
-            method: 'GET',
-            url: '/api/spac/facturacion/facturacion/enRevision',
-        })
-        .then((response) => {
-            expect(response.status).to.eq(200)
-            console.log(response.body)
-        })
-    })
-
-    it('[transferibles] status 200', () => {
-        cy.get('@jsession').request({
-            method: 'GET',
-            url: '/api/spac/facturacion/facturacion/transferibles',
-        })
-        .then((response) => {
-            expect(response.status).to.eq(200)
-            console.log(response.body)
-        })
-    })
-
-
-
-
 
 
 
